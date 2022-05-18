090c7e6acd644e3abfc6d2fdbe9282e3ce589edc
Jonas <56507045+jonas-koll@users.noreply.github.com>
2022-05-18 16:01:12 +0200

    Revert "+ Added IngameChat support"

--- src/main/java/de/stornierung/streamchat/StreamChat.java
+++ src/main/java/de/stornierung/streamchat/StreamChat.java
@@ -1,24 +1,20 @@
 package de.stornierung.streamchat;
 
 import de.stornierung.streamchat.api.TwitchBot;
-import de.stornierung.streamchat.listener.IngameChat;
 import net.labymod.api.LabyModAddon;
 import net.labymod.main.LabyMod;
 import net.labymod.settings.elements.*;
 import net.labymod.utils.Consumer;
 import net.labymod.utils.Material;
 import net.labymod.utils.ServerData;
-import net.minecraft.client.Minecraft;
 
 import java.util.List;
 
 public class StreamChat extends LabyModAddon {
     public static boolean enabled;
-    public static String twitchChannel;
-    public static String ingamePrefix;
+    public static String twitchchannel;
     public static String prefix;
     public static boolean twitchenabled;
-    public static boolean chatenabled;
     public static String twitchtoken;
     public static TwitchBot bot;
 
@@ -29,15 +25,14 @@ public class StreamChat extends LabyModAddon {
 
     @Override
     public void onEnable() {
-        System.out.println("## StreamChat+ | by unordentlich ##");
+        System.out.println("## StreamChat+ | by Stornierung ##");
 
         this.getApi().registerForgeListener(this);
-        this.getApi().getEventManager().register(new IngameChat());
         this.getApi().getEventManager().registerOnJoin(new Consumer<ServerData>() {
             @Override
             public void accept(ServerData serverData) {
                 TwitchBot.startBot();
-                if (twitchenabled) {
+                if(twitchenabled) {
                     if (!token_working) {
                         System.out.println("StreamChat+ | Addon couldn't connect to your twitch profile! :(");
                         LabyMod.getInstance().displayMessageInChat("§eStream§7Chat§6+ §8» §cAddon couldn't connect to your twitch profile! :(");
@@ -56,20 +51,16 @@ public class StreamChat extends LabyModAddon {
 
     @Override
     public void loadConfig() {
-        if (!getConfig().has("enabled")) getConfig().addProperty("enabled", true);
-        if (!getConfig().has("twitchtoggle")) getConfig().addProperty("twitchtoggle", true);
-        if (!getConfig().has("twitchchannel")) getConfig().addProperty("twitchchannel", "");
-        if (!getConfig().has("prefix")) getConfig().addProperty("prefix", "§5✪ §d%user% §8> §7%message%");
-        if (!getConfig().has("twitchtoken")) getConfig().addProperty("twitchtoken", "");
-        if (!getConfig().has("chattoggle")) getConfig().addProperty("chattoggle", false);
-        if (!getConfig().has("chatprefix")) getConfig().addProperty("chatprefix", "##");
+        if(!getConfig().has("enabled")) getConfig().addProperty("enabled", true);
+        if(!getConfig().has("twitchtoggle")) getConfig().addProperty("twitchtoggle", true);
+        if(!getConfig().has("twitchchannel")) getConfig().addProperty("twitchchannel", "");
+        if(!getConfig().has("prefix")) getConfig().addProperty("prefix", "§5✪ §d%user% §8> §7%message%");
+        if(!getConfig().has("twitchtoken")) getConfig().addProperty("twitchtoken", "");
         enabled = getConfig().get("enabled").getAsBoolean();
         twitchenabled = getConfig().get("twitchtoggle").getAsBoolean();
-        twitchChannel = getConfig().get("twitchchannel").getAsString();
+        twitchchannel = getConfig().get("twitchchannel").getAsString();
         prefix = getConfig().get("prefix").getAsString();
         twitchtoken = getConfig().get("twitchtoken").getAsString();
-        chatenabled = getConfig().get("chattoggle").getAsBoolean();
-        ingamePrefix = getConfig().get("chatprefix").getAsString();
     }
 
     @Override
@@ -79,11 +70,11 @@ public class StreamChat extends LabyModAddon {
         subSettings.add(new HeaderElement("§7Support? §8» §ejonas#6789"));
 
         /* ENABLED TOGGLE */
-        subSettings.add(new BooleanElement("Enabled",
-                new ControlElement.IconData(Material.EMERALD), new Consumer<Boolean>() {
+        subSettings.add( new BooleanElement( "Enabled",
+                new ControlElement.IconData( Material.EMERALD ), new Consumer<Boolean>() {
             @Override
             public void accept(Boolean enabled) {
-                StreamChat.enabled = enabled;
+                StreamChat.this.enabled = enabled;
 
                 StreamChat.this.getConfig().addProperty("enabled", enabled);
                 StreamChat.this.saveConfig();
@@ -92,12 +83,12 @@ public class StreamChat extends LabyModAddon {
         }, enabled));
 
         /* PREFIX */
-        StringElement prefixStringElement = new StringElement("Prefix",
-                new ControlElement.IconData(Material.BOOK),
+        StringElement prefixStringElement = new StringElement( "Prefix",
+                new ControlElement.IconData( Material.BOOK ),
                 prefix.replace("§", "&"), new Consumer<String>() {
             @Override
             public void accept(String prefix) {
-                StreamChat.prefix = prefix.replace("&", "§");
+                StreamChat.this.prefix = prefix.replace("&", "§");
 
                 StreamChat.this.getConfig().addProperty("prefix", prefix.replace("&", "§"));
                 StreamChat.this.saveConfig();
@@ -115,83 +106,45 @@ public class StreamChat extends LabyModAddon {
                 new ControlElement.IconData(Material.REDSTONE), new Consumer<Boolean>() {
             @Override
             public void accept(Boolean enabled) {
-                twitchenabled = enabled;
+                StreamChat.this.twitchenabled = enabled;
 
                 StreamChat.this.getConfig().addProperty("twitchtoggle", enabled);
                 StreamChat.this.saveConfig();
                 loadConfig();
             }
-        }, twitchenabled));
+            }, twitchenabled));
 
         /* TWITCH USERNAME */
-        StringElement channelStringElement = new StringElement("Twitch Username",
-                new ControlElement.IconData(Material.PAPER),
-                twitchChannel, new Consumer<String>() {
+        StringElement channelStringElement = new StringElement( "Twitch Username",
+                new ControlElement.IconData( Material.PAPER ),
+                twitchchannel, new Consumer<String>() {
             @Override
             public void accept(String username) {
-                twitchChannel = username;
+                StreamChat.this.twitchchannel = username;
 
                 StreamChat.this.getConfig().addProperty("twitchchannel", username);
                 StreamChat.this.saveConfig();
                 loadConfig();
 
-                if (Minecraft.getMinecraft().isSingleplayer() || Minecraft.getMinecraft().getCurrentServerData() != null) {
-                    LabyMod.getInstance().displayMessageInChat("§eStream§7Chat§6+ §8» §7Please re-enter the server/world to load all changes.");
-                }
+                LabyMod.getInstance().displayMessageInChat("§eStream§7Chat§6+ §8» §7Please re-enter the server/world to load all changes.");
             }
         });
         subSettings.add(channelStringElement);
 
         /* TWITCH OAUTH TOKEN */
-        StringElement tokenStringElement = new StringElement("Twitch Token",
-                new ControlElement.IconData(Material.COMMAND),
+        StringElement tokenStringElement = new StringElement( "Twitch Token",
+                new ControlElement.IconData( Material.COMMAND ),
                 twitchtoken, new Consumer<String>() {
             @Override
             public void accept(String token) {
-                twitchtoken = token;
+                StreamChat.this.twitchtoken = token;
 
                 StreamChat.this.getConfig().addProperty("twitchtoken", token);
                 StreamChat.this.saveConfig();
                 loadConfig();
-
-                if (Minecraft.getMinecraft().isSingleplayer() || Minecraft.getMinecraft().getCurrentServerData() != null) {
-                    LabyMod.getInstance().displayMessageInChat("§eStream§7Chat§6+ §8» §7Please re-enter the server/world to load all changes.");
-                }
             }
         });
         subSettings.add(tokenStringElement);
-
-        /* TWITCH TITLE */
-        HeaderElement placeholder = new HeaderElement(" ");
-        subSettings.add(placeholder);
-
-        /* TWITCH-INGAME CHAT TOGGLE */
-        subSettings.add(new BooleanElement("Chat via Ingame-Messages",
-                new ControlElement.IconData(Material.MAP), new Consumer<Boolean>() {
-            @Override
-            public void accept(Boolean enabled) {
-                chatenabled = enabled;
-
-                StreamChat.this.getConfig().addProperty("chattoggle", enabled);
-                StreamChat.this.saveConfig();
-                loadConfig();
-            }
-        }, chatenabled));
-
-        /* INGAME CHATTER */
-        StringElement ingameChatPrefix = new StringElement("Chat Prefix",
-                new ControlElement.IconData(Material.PAPER),
-                ingamePrefix, new Consumer<String>() {
-            @Override
-            public void accept(String prefix) {
-                ingamePrefix = prefix;
-
-                StreamChat.this.getConfig().addProperty("chatprefix", prefix);
-                StreamChat.this.saveConfig();
-                loadConfig();
-            }
-        });
-        subSettings.add(ingameChatPrefix);
 
         /* SUPPORT NOTICE */
         subSettings.add(new HeaderElement("§7§oYou can get your twitch token from https://twitchapps.com/tmi/", 0.7));
